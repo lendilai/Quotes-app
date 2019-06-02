7b1ef3886de52e860b207d074a4e8d7f28dcdf02
lendilai <ngethenan768@gmail.com>
2019-06-02 12:28:59 +0300

    Adds routing components and a nav-bar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
+
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "post", component: PostComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, PostComponent];
