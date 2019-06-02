e2df1741a1729ea4392c1c546be4fed6bcb08fa0
lendilai <ngethenan768@gmail.com>
2019-06-02 14:23:11 +0300

    Does data binding to toggle between the descriptions

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,3 +1,5 @@
 export class Quotes {
-  constructor(public author:string, public description:string, public newDate:Date){}
+  constructor(public author:string, public description:string, public newDate:Date){
+    this.showQuote=false;
+  }
 }
